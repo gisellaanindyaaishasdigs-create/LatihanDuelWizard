d6ee75d638dbecdd7fade68fd8e76de5ee591c8d
kevella95 <gisellaanindyaaisha.sdigs@gmail.com>
2026-03-04 10:30:26 +0700

    Initial commit

---
+++ LatihanDuelWizard/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Wizard
+{
+    //deklarasi fied
+    public string Name;
+    public int Energy;
+    public int Damage;
+
+    //constructor
+    public Wizard(string name, int damage)
+    {
+        Name = name;
+        Energy = 100;
+        Damage = damage;
+    }
+
+    //deklarasi method
+    public void ShowStats()
+    {
+        Console.WriteLine(($"Name: {Name}");
+        Console.WriteLine($"Energy:{Energy}\n");
+    }
+
+    public void Attack(Wizard enemyObj)
+    {
+        enemyObj.Energy -= Damage;
+        Console.WriteLine($"{Name} menyerang {enemyObj.Name}");
+        Console.WriteLine($"Sisa energi {enemyObj.Name} {enemyObj.Energy}\n");
+    }
+
+}
