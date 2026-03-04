83a9ec93adcfa6b4466883ccf180e208761467f3
kevella95 <gisellaanindyaaisha.sdigs@gmail.com>
2026-03-04 11:49:12 +0700

    menambah mekanisme pilihan aksi untuk pemain

--- LatihanDuelWizard/Program.cs
+++ LatihanDuelWizard/Program.cs
@@ -4,6 +4,37 @@ using System.Xml.Linq;
 Wizard wizardA = new Wizard("Mak Lampir", 5);
 Wizard wizardB = new Wizard("Nirmala", 10);
 
+while (true)
+{
+    Console.WriteLine($"1. {wizardA.Name} menyerang {wizardB.Name}");
+    Console.WriteLine($"2. {wizardB.Name} menyerang {wizardA.Name}");
+    Console.WriteLine($"3. {wizardA.Name} melakukan Heal");
+    Console.WriteLine($"4. {wizardB.Name} melakukan Heal");
+
+    Console.Write("\nPilihan (1,2,3,4): ");
+    pilihan = Console.ReadLine();
+
+    if (pilihan == "1") wizardA.Attack(wizardB);
+    else if (pilihan == "2") wizardB.Attack(wizardA);
+    else if (pilihan == "3") wizardA.Heal();
+    else if (pilihan == "4") wizardB.Heal();
+    else Console.WriteLine("Pilihan tidak valid");
+
+    if (wizardA.Energy <= 0)
+    {
+        Console.WriteLine($"{wizardA.Name} kalah!");
+        Console.WriteLine($"{wizardB.Name} menang!");
+        break;
+    }
+    else if (wizardB.Energy <= 0)
+    {
+        Console.WriteLine($"{wizardB.Name} kalah!");
+        Console.WriteLine($"{wizardA.Name} menang!");
+        break;
+    }
+}
+    }
+}
 
 public class Wizard
 {
@@ -34,6 +65,9 @@ public class Wizard
         Console.WriteLine($"Sisa energi {enemyObj.Name} {enemyObj.Energy}\n");
     }
 
+    string pilihan;
+
+}
 
     public void Heal()
     {
@@ -58,4 +92,6 @@ public class Wizard
         }
 
     }
+
+    
 
