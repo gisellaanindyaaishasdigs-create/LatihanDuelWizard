1caabd976a67975ceb8b3a20ac07a66110b0b701
kevella95 <gisellaanindyaaisha.sdigs@gmail.com>
2026-03-04 11:40:09 +0700

    Menambahkan method Heal

--- LatihanDuelWizard/Program.cs
+++ LatihanDuelWizard/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Xml.Linq;
+
 Wizard wizardA = new Wizard("Mak Lampir", 5);
 Wizard wizardB = new Wizard("Nirmala", 10);
 
@@ -32,4 +34,28 @@ public class Wizard
         Console.WriteLine($"Sisa energi {enemyObj.Name} {enemyObj.Energy}\n");
     }
 
-}
+
+    public void Heal()
+    {
+        if (Energy > 100)
+        {
+
+            Console.WriteLine("Gagal melakukan Heal, Energy sudah mecapai maksimal");
+        }
+        else
+        {
+            if (Energy > 95)
+            {
+                Energy = 100;
+            }
+            else
+            {
+                Energy = +5;
+            }
+            Console.WriteLine($"{Name} berhasil melakukan heal. energi meningkat menjadi (Energy)");
+
+
+        }
+
+    }
+
